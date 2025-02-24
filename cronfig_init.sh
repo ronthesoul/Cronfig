f1b1b93087268836b1fa6ade6729dfaeb24dbf9c
Ron Negrov <m4gapower@gmail.com>
2025-02-24 08:31:54 +0200

    Added features

--- cronfig_init.sh
+++ cronfig_init.sh
@@ -5,6 +5,24 @@ echo "alias ..='cd ..'" >> ~/.bashrc
 echo "alias ...='cd ../..'" >> ~/.bashrc
 echo "alias ....='cd ../../..'" >> ~/.bashrc
 echo "alias home='cd ~'" >> ~/.bashrc
+
+#Memory managment
+echo "alias mem='free -m -l -t'" >> ~/.bashrc
+echo "alias topmem='ps aux --sort=-%mem | head -10'" >> ~/.bashrc
+echo "alias topcpu='ps aux --sort=-%cpu | head -10''" >> ~/.bashrc
+
+#Disk space
+echo "alias disk='df -h'" >> ~/.bashrc
+echo "alias space='du -sh *'" >> ~/.bashrc
+echo "alias bigfiles='find . -type f -exec du -h {} + | sort -rh | head -10'" >> ~/.bashrc
+
+#Networking
+echo "alias myip='curl ifconfig.me'" >> ~/.bashrc
+echo "alias pingg='ping google.com -c 5'" >> ~/.bashrc
+echo "alias ports='netstat -tulnp'" >> ~/.bashrc
+
+
+
 source ~/.bashrc
 
 
