641e646176109431889f9440f3002a5b487bf3c2
Ron Negrov <m4gapower@gmail.com>
2025-02-24 08:18:36 +0200

    cd commands

---
+++ cronfig_init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#cd aliases
+echo "alias ..='cd ..'" >> ~/.bashrc
+echo "alias ...='cd ../..'" >> ~/.bashrc
+echo "alias ....='cd ../../..'" >> ~/.bashrc
+echo "alias home='cd ~'" >> ~/.bashrc
+source ~/.bashrc
+
+
+
