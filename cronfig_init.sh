b2a2f6091858dd65d4dc35aed02fc4765c855a5f
Ron Negrov <m4gapower@gmail.com>
2025-02-26 21:33:28 +0200

    Added vim settings

--- cronfig_init.sh
+++ cronfig_init.sh
@@ -10,6 +10,10 @@
 PS1_VALUE='PS1='\''\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[01;33m\]$(git rev-parse --abbrev-ref HEAD 2>/dev/null | sed "s/\(.*\)/ [\1]/")\[\033[00m\]\n$ '\'''
 echo "$PS1_VALUE" >> ~/.bashrc
 
+#System configs
+echo "Lazarus" > /etc/hostname 2>&1
+
+
 
 #bin aliases
 echo "alias ..='cd ..'" >> ~/.bashrc
@@ -18,6 +22,7 @@ echo "alias ....='cd ../../..'" >> ~/.bashrc
 echo "alias home='cd ~'" >> ~/.bashrc
 echo "alias cp='cp -v'" >> ~/.bashrc
 echo "alias mv='mv -v'" >> ~/.bashrc
+echo "alias brcs='source ~/.bashrc'" >> ~/.bashrc
 
 #Memory managment
 echo "alias mem='free -m -l -t'" >> ~/.bashrc
@@ -45,7 +50,58 @@ echo "alias gp='git push -u'" >> ~/.bashrc
 echo "alias gitcon='ssh -T git@github.com'" >> ~/.bashrc
 
 
+#Vim configs
 
+if ! dpkg -l | grep -q "^ii  vim "; then
+    sudo apt install -y vim
+fi
+
+if ! dpkg -l | grep -q "^ii  vim-gtk3 "; then
+    sudo apt install vim-gtk3 -y
+fi
+
+echo 'export EDITOR=vim' >> ~/.bashrc
+
+mkdir -p ~/.vim/colors
+curl -o ~/.vim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+
+cat << 'EOF' >> ~/.vimrc
+set number
+set mouse=a
+set tabstop=4
+set shiftwidth=4
+set expandtab
+set showtabline=1
+set clipboard=unnamedplus  
+set incsearch
+set hlsearch
+set ruler
+set showcmd
+set wildmenu
+set showmatch
+
+syntax on
+set completeopt=menuone,noselect
+
+" Save and Quit shortcuts
+nnoremap <C-s> :w<CR>
+nnoremap <C-q> :wq<CR>
+
+" Copy Paste shortcuts
+vnoremap <C-c> "+y
+nnoremap <C-p> "+p
+inoremap <C-p> <C-r>+
+cnoremap <C-p> <C-r>+
+tnoremap <C-p> <C-r>+
+
+" Map ctrl left and right to move between tabs
+nnoremap <C-Right> :tabnext<CR>
+nnoremap <C-Left> :tabprev<CR>
+
+
+set termguicolors
+colorscheme github
+EOF
 
 
 #Functions
