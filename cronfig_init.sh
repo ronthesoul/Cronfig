dbc34323f088e47d95868b6eb1c6e4ddf4c7c470
Ron Negrov <m4gapower@gmail.com>
2025-06-17 22:01:38 +0300

    added some changes

--- cronfig_init.sh
+++ cronfig_init.sh
@@ -24,95 +24,6 @@ PS1_VALUE='PS1='\''\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[
 echo "$PS1_VALUE" >> ~/.bashrc
 
 
-
-#bin aliases
-echo "alias ..='cd .. && ls'" >> ~/.bashrc
-echo "alias ...='cd ../.. && ls'" >> ~/.bashrc
-echo "alias ....='cd ../../.. && ls'" >> ~/.bashrc
-echo "alias home='cd ~ && ls'" >> ~/.bashrc
-echo "alias cp='cp -v'" >> ~/.bashrc
-echo "alias mv='mv -v'" >> ~/.bashrc
-echo "alias brcs='source ~/.bashrc'" >> ~/.bashrc
-echo "alias vbr='vim ~/.bashrc'" >> ~/.bashrc
-
-#Memory managment
-echo "alias mem='free -m -l -t'" >> ~/.bashrc
-echo "alias topmem='ps aux --sort=-%mem | head -10'" >> ~/.bashrc
-echo "alias topcpu='ps aux --sort=-%cpu | head -10'" >> ~/.bashrc
-
-#Disk space
-echo "alias disk='df -h'" >> ~/.bashrc
-echo "alias space='du -sh *'" >> ~/.bashrc
-
-#Networking
-echo "alias myip='curl ifconfig.me'" >> ~/.bashrc
-echo "alias pingg='ping google.com -c 5'" >> ~/.bashrc
-echo "alias ports='ss -tulpn'" >> ~/.bashrc
-echo "alias ipc='ip -br -c  addr'" >> ~/.bashrc
-echo "alias ipd='ip route show default'" >> ~/.bashrc
-echo "alias ipn='cat /etc/resolv.conf | grep nameserver'" >> ~/.bashrc
-
-#Git 
-echo "alias gl='git log --oneline --graph --decorate --all'" >> ~/.bashrc
-echo "alias gr='git remote -v'" >> ~/.bashrc
-echo "alias gpf='git fetch origin && git pull --rebase'" >> ~/.bashrc
-echo "alias gp='git push -u'" >> ~/.bashrc
-echo "alias gitcon='ssh -T git@github.com'" >> ~/.bashrc
-echo "alias gc='git commit'" >> ~/.bashrc
-echo "alias ga='git add'" >> ~/.bashrc
-echo "alias gb='git branch'" >> ~/.bashrc
-echo "alias gs='git status'" >> ~/.bashrc
-
-#nginx commands
-echo "alias nxs='service nginx status'" >> ~/.bashrc
-echo "alias nxr='sudo service nginx restart'" >> ~/.bashrc
-echo "alias nxstart='sudo service nginx start'" >> ~/.bashrc
-echo "alias nxstop='sudo service nginx stop'" >> ~/.bashrc
-echo "alias cdse='cd /etc/nginx/sites-enabled'" >> ~/.bashrc
-
-#docker commands
-echo "alias drad='docker rm $(docker ps -a -q -f "status=exited")'" >> ~/.bashrc
-echo "alias drld='docker ps -a -q -f "status=exited"'" >> ~/.bashrc
-echo "alias dclean='docker system prune -f'" >> ~/.bashrc
-echo "alias dstopall='docker stop $(docker ps -q)'" >> ~/.bashrc
-echo "alias dps='docker ps'" >> ~/.bashrc
-
-cat << 'EOF' >> ~/.bashrc
-function get_container_ip(){
-docker inspect $1 | jq -r '.[0].NetworkSettings.Networks.$1.IPAddress'
-}
-EOF
-
-cat << 'EOF' >> ~/.bashrc
-function create_sandbox(){
-malware_volume='/var/sandbox/malware'
-
-if [[ ! -e $malware_volume ]]; then
-mkdir -p $malware_volume
-fi
-
-docker run -it \
-  --rm \
-  --cap-drop=ALL \
-  --security-opt=no-new-privileges \
-  --read-only \
-  --network none \
-  -v $malware_volume:/malware:ro \
-  $1
-}
-EOF
-cat << 'EOF' >> ~/.bashrc
-function denter(){
-docker exec -it "$1" /bin/bash
-}
-EOF
-cat << 'EOF' >> ~/.bashrc
-function dpurge(){
-    docker stop $(docker ps -q)
-    docker rm $(docker ps -a -q -f "status=exited")
-}
-EOF
-
 #Vim configs
 
 if ! dpkg -l | grep -q "^ii  vim "; then
@@ -211,46 +122,7 @@ chmod +x ~/.base16config/base16-shell/scripts/base16-default-dark.sh
 echo 'source ~/.base16config/base16-shell/scripts/base16-default-dark.sh' >> ~/.bashrc
 
 
-#Functions
-cat << 'EOF' >> ~/.bashrc 
-function change_repo (){
-repo_profile=$1
-repo_name=$2
-
-git remote add origin git@github.com:$repo_name/$repo_name
-git remote set-url origin git@github.com:$repo_profile/$repo_name
-}
-EOF
-
-
-cat << EOF >> ~/.bashrc
-if [[ ! -e ~/.bashrc_backup ]]; then
-    mkdir -p ~/.bashrc_backup
-fi
-
-cp ~/.bashrc ~/.bashrc_backup/bashrc
-EOF
-
-cat << EOF >> ~/.bashrc
-function cl (){
-cd $1
-ls
-}
-EOF
-
-cat << 'EOF' >> ~/.bashrc 
-function brcs () { 
-    local backup_dir="$HOME/.bashrc_backup"
-    mkdir -p "$backup_dir"
-    if [[ -f "$backup_dir/bashrc_prev" ]]; then
-        mv "$backup_dir/bashrc_prev" "$backup_dir/bashrc_prev2"
-    fi
-
-    if [[ -f "$HOME/.bashrc" ]]; then
-        cp "$HOME/.bashrc" "$backup_dir/bashrc_prev"
-    fi
-
-    source "$HOME/.bashrc"
-}
-EOF
-
+curl -o alias_bashrc https://raw.githubusercontent.com/ronthesoul/Cronfig/main/new_config/alias_bashrc
+curl -o _func_bashrc https://raw.githubusercontent.com/ronthesoul/Cronfig/main/new_config/_func_bashrc
+source "$HOME/.alias_bashrc"
+source "$HOME/.func_bashrc"
